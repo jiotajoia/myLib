77f34f29079c238feae586b5f901304484c045ab
jiotajoia <bleibeducuz@gmail.com>
2024-11-08 12:39:35 -0300

    first commit

---
+++ app/Http/Controllers/Painel/PainelController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Painel;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PainelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $title = "Página Inicial";
+        return view('painel.paginaInicial',compact('title'));
+    }
+
+    public function explorar(){
+        $title = "Explorar";
+        return view('painel.explorar',compact('title'));
+    }
+
+    public function biblioteca(){
+        $title = "Biblioteca";
+        return view('painel.biblioteca', compact('title'));
+    }
+}
