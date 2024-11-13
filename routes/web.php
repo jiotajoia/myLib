10bb1b8da7b78bdd5aee8abfae2dda21b1269ae3
jiotajoia <bleibeducuz@gmail.com>
2024-11-13 11:18:48 -0300

    tirou a rota dashboard

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,15 @@ Route::get('/', function () {
 
 Route::get('/contato', [SiteController::class, 'contact']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/painel/paginaInicial',[PainelController::class, 'index'])->name('paginaInicial');//pagina inicial
     Route::get('painel/explorar',[PainelController::class,'explorar'])->name('explorar');//explorar
     Route::get('/painel/biblioteca', [PainelController::class, 'biblioteca'])->name('biblioteca');//biblioteca
-    Route::get('/painel/meusLivros', [PainelController::class, 'meusLivros'])->name('meusLivros');
+    Route::get('/painel/meusLivros', [PainelController::class, 'meusLivros'])->name('meusLivros');//meus livros
     //default de autenticação
     Route::get('/painel/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/painel/profile', [ProfileController::class, 'update'])->name('profile.update');
