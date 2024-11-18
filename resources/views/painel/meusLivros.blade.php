f45733bd8069c45f087f3a0b6dd08a15dcf35ab3
jiotajoia <bleibeducuz@gmail.com>
2024-11-18 12:51:40 -0300

    inserção da nav pill

--- resources/views/painel/meusLivros.blade.php
+++ resources/views/painel/meusLivros.blade.php
@@ -10,6 +10,63 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h3>{{ $title }}</h3>
                 </div>
+
+                <div class="nav-wrapper position-relative end-0">
+                    <ul class="nav nav-pills nav-fill p-1" role="tablist">
+                       <li class="nav-item">
+                          <a class="nav-link mb-0 px-0 py-1 active" data-bs-toggle="tab" href="" role="tab" aria-controls="profile" aria-selected="true">
+                          Publicados
+                          </a>
+                       </li>
+                       <li class="nav-item">
+                          <a class="nav-link mb-0 px-0 py-1" data-bs-toggle="tab" href="" role="tab" aria-controls="dashboard" aria-selected="false">
+                          Rascunhos
+                          </a>
+                       </li>
+                     </ul>
+                 </div>
+
+                <div class="d-flex justify-content-center align-items-center">
+
+                    <div class="col-xl-6 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card mb-4">
+                            <div class="card-header p-2 ps-3">
+                                <div class="d-flex justify-content-between">
+                                    <div>
+                                        <p class="text-small mb-0 text-capitalize">Livro</p>
+                                        <h4 class="mb-0">Descrição</h4>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <hr class="dark horizontal my-0">
+                            <div class="card-footer p-2 ps-3">
+                                <p class="mb-0 text-small"><span class="text-success font-weight-bolder">Tags</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-6 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card mb-4">
+                            <div class="card-header p-2 ps-3">
+                                <div class="d-flex justify-content-between">
+                                    <div>
+                                        <p class="text-small mb-0 text-capitalize">Livro</p>
+                                        <h4 class="mb-0">Descrição</h4>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <hr class="dark horizontal my-0">
+                            <div class="card-footer p-2 ps-3">
+                                <p class="mb-0 text-small"><span class="text-success font-weight-bolder">Tags</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     @endsection
