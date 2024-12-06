0e33854f3d2f7716317ee561652901c65698d60e
jiotajoia <bleibeducuz@gmail.com>
2024-12-06 09:34:00 -0300

    mudei a title pra myLib

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
-    <title>myLib</title>
+    <title>myLib</title> 
 
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
 
